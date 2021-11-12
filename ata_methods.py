d2e446b6b49d2ff5873e88df2537af88196a4a57
ranjabi <fikriranjabi02@gmail.com>
2021-11-12 14:15:25 +0700

    add ata method

---
+++ ata_methods.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+import qr_decomposition
+import numpy as np
+from PIL import Image
+
+def find_v(matrix):
+    a = matrix
+    at = a.transpose()
+    v = np.dot(at,a)
+    return v
+
+def find_svd(a):
+    def find_ata(a):
+        at = a.transpose()
+        return np.dot(at,a)
+    v = find_ata(a)
+    rows = len(a)
+    cols = len(a[0])
+    
+    # pake qr method Gilang
+    eval,evec = qr_decomposition.find_eig_qr(v) 
+    
+    # pake library eigen
+    # eval,evec = np.linalg.eig(v)
+    
+    eigenValues = eval
+    eigenVectors = evec
+    idx = eigenValues.argsort()[::-1]   
+    eigenValues = eigenValues[idx]
+    eigenVectors = eigenVectors[:,idx]
+    
+    singular = np.zeros(len(eigenValues))
+    for i in range(len(eigenValues)):
+        if (eigenValues[i] < 1e-16):
+            singular[i] = 0
+        else:
+            singular[i] = math.sqrt(eigenValues[i])
+    
+    print((-1) * eigenVectors.transpose())
+    vT = (-1) * eigenVectors.transpose()
+    v = (-1) * eigenVectors
+
+    matrixs = np.zeros((rows,cols))
+    for i in range(np.linalg.matrix_rank(a)):
+        matrixs[i][i] = math.sqrt(eigenValues[i])
+
+    matu = np.zeros((rows,rows))
+    for j in range(rows):
+        matu[:,j] = (np.matmul(a,vT[j])) / math.sqrt(eigenValues[j])
+    svd = np.dot(matu,np.dot(matrixs,vT))
+    return matu,singular,vT
--- k_svd.py
+++ k_svd.py
@@ -2,9 +2,14 @@ import svd
 import numpy as np
 from PIL import Image
 import math
+import ata_methods
 
 def k_svd(matrix,k): # Melakukan aproksimasi terhadap matriks gambar berdasarkan k dari input user
-    U,Sig,Vt = np.linalg.svd(matrix) # melakukan svd terhadap matriks
+    # U,Sig,Vt = np.linalg.svd(matrix) # melakukan svd terhadap matriks
+    U,Sig,Vt = ata_methods.find_svd(matrix)
+    # print("u\n", U.shape)
+    # print("Sig\n", Sig.shape)
+    # print("Vt\n", Vt.shape)
     
     # mengaproksimasi matriks hasil svd berdasarkan input k
     min_mat = np.dot(U[:,:k], np.dot(np.diag(Sig[:k]), Vt[:k,:]))
