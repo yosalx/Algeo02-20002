534032e846b81ca832547827569d98cfae4bf1b7
yosalx <yoseph141002@gmail.com>
2021-11-12 14:51:21 +0700

    update fix

--- qr_decomposition.py
+++ qr_decomposition.py
@@ -1,53 +1,52 @@
-#Edward Harianto
 import numpy as np
 
-def isDiagonal(mat):
-	for i in range(len(mat)):
-		for j in range(len(mat)):
-			if(i == j):
-				continue
-			else:
-				if (abs(mat[i][j]) > 0.001):
-					return False
-	return True
+# def isDiagonal(mat):
+# 	for i in range(len(mat)):
+# 		for j in range(len(mat)):
+# 			if(i == j):
+# 				continue
+# 			else:
+# 				if (abs(mat[i][j]) > 0.001):
+# 					return False
+# 	return True
 
-def qrAlgorithm(mat):
-	num = 0
+# def qrAlgorithm(mat):
+# 	num = 0
 	
-	while(True):
-		q,r = np.linalg.qr(mat)
-		mat = np.dot(r,q)
+# 	while(True):
+# 		q,r = np.linalg.qr(mat)
+# 		mat = np.dot(r,q)
 		
-		if(isDiagonal(mat)):
-			break
-		num+=1
+# 		if(isDiagonal(mat)):
+# 			break
+# 		num+=1
 
-	print('QR Factoriztion matrix')
-	print(mat)
-	print(str(num) + ' iterations')
+# 	print('QR Factoriztion matrix')
+# 	print(mat)
+# 	print(str(num) + ' iterations')
 	
-	count = 1
-	for i in range(len(mat)):
-		for j in range(len(mat)):
-			if (i == j):
-				print("lambda " + str(count) + ": " + str(mat[i][j]))
-				count+=1
+# 	count = 1
+# 	for i in range(len(mat)):
+# 		for j in range(len(mat)):
+# 			if (i == j):
+# 				print("lambda " + str(count) + ": " + str(mat[i][j]))
+# 				count+=1
 
-def qr_factorization(A):
-   #source: http://mlwiki.org/index.php/Gram-Schmidt_Process
-    m, n = A.shape
-    Q = np.zeros((m, n))
-    R = np.zeros((n, n))
-    for j in range(n):
-        v = A[:, j]
-        for i in range(j - 1):
-            q = Q[:, i]
-            R[i, j] = q @ v
-            v = v - R[i, j] * q
-        norm = np.linalg.norm(v)
-        Q[:, j] = v / norm
-        R[j, j] = norm
-    return Q, R
+# def qr_factorization(A):
+#        #source: http://mlwiki.org/index.php/Gram-Schmidt_Process
+#     m, n = A.shape
+#     Q = np.zeros((m, n))
+#     R = np.zeros((n, n))
+#     for j in range(n):
+#         v = A[:, j]
+#         for i in range(j - 1):
+#             q = Q[:, i]
+#             R[i, j] = q @ v
+#             v = v - R[i, j] * q
+#         norm = np.linalg.norm(v)
+#         Q[:, j] = v / norm
+#         R[j, j] = norm
+#     return Q, R
 
 def find_eig_qr(A):
     pQ = np.eye(A.shape[0])
@@ -57,32 +56,3 @@ def find_eig_qr(A):
             pQ = pQ @ Q;
             X = R @ Q;
     return np.diag(X), pQ
-
-
-
-# A = np.array([[2,0,2],[0,3,0],[2,0,2],[0,3,0],[2,0,2]])
-# AT = np.transpose(A)
-# AAT = np.matmul(A,AT)
-# matrix = np.random.random_integers(0, 100, (5, 1000))
-# from datetime import datetime
-# start_time = datetime.now()	
-# x,y = find_eig_qr(AAT)
-
-# u,Sig,V = np.linalg.svd(A)
-<<<<<<< HEAD
-
-=======
-# print(Sig)
-
-# print("eigen value\n")
->>>>>>> e61e8d9ae3845f44ad0df09a3135ea248f68f5ae
-# print(x)
-# print("eigenvector")
-# print(y)
-# print("svd")
-# print(u)
-# end_time = datetime.now()
-# print('Duration: {}'.format(end_time - start_time))
-
-
-
