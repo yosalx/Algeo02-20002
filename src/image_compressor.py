2508d72464268873b0dafc107a96b07f0949336e
yosalx <yoseph141002@gmail.com>
2021-11-14 21:43:58 +0700

    update for png

--- src/image_compressor.py
+++ src/image_compressor.py
@@ -1,10 +1,10 @@
 import image_processing
+import image_processing2
 import k_svd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from PIL import Image
 import os
-import math
 
 def img_comp(filename,percent): # parameter input adalah nama file dan jumlah singular value yang akan digunakan
     
@@ -14,13 +14,13 @@ def img_comp(filename,percent): # parameter input adalah nama file dan jumlah si
     
     comppresed = [] # menyiapkan tempat untuk matriks rgb dari gambar yang akan disederhanakan dengan proses svd
     
-    r = np.clip((k_svd.k_svd(r,percent)),0,255) # mengaproksimasi 
-    g = np.clip((k_svd.k_svd(g,percent)),0,255) # menambahkan matriks g sederhana
-    b = np.clip((k_svd.k_svd(b,percent)),0,255) # menambahkan matriks b sederhana
+    r = np.clip((k_svd.k_svd(r,percent)),0,255) # mengaproksimasi matriks r
+    g = np.clip((k_svd.k_svd(g,percent)),0,255) # mengaproksimasi matriks g
+    b = np.clip((k_svd.k_svd(b,percent)),0,255) # mengaproksimasi matriks b
 
-    comppresed.append(r)
-    comppresed.append(g)
-    comppresed.append(b)
+    comppresed.append(r) # menambahkan matriks r sederhana
+    comppresed.append(g) # menambahkan matriks g sederhana
+    comppresed.append(b) # menambahkan matriks b sederhana
     
     def merge_rgb(shape,mat): # deklarasi fungsi untuk penggabungan kembali matriks rgb
         merge = np.zeros(shape) # menyiapkan matriks 0 dengan ukuran sesusai ukuran matriks dari gambar
@@ -47,6 +47,25 @@ def img_comp(filename,percent): # parameter input adalah nama file dan jumlah si
     # plt.imshow(save)
     # plt.show()
     
+def img_comp_png(filename,percent):
+    
+    # mengubah gambar yang akan dikompres menjadi matriks, dipisahkan menjadi matriks rgba
+    r,g,b,alp = image_processing2.image_matrix2(filename)
+    
+    r = np.clip((k_svd.k_svd(r,percent)),0,255) # mengaproksimasi matriks r
+    g = np.clip((k_svd.k_svd(g,percent)),0,255) # mengaproksimasi matriks g
+    b = np.clip((k_svd.k_svd(b,percent)),0,255) # mengaproksimasi matriks b
+    
+    #mengubah matriks menjadi bentuk gambar dalam mode grayscale (convert("L"))
+    r_im = (Image.fromarray(r)).convert("L")
+    g_im = (Image.fromarray(g)).convert("L")
+    b_im = (Image.fromarray(b)).convert("L")
+    alp_im = (Image.fromarray(alp)).convert("L")
+    
+    save_png = Image.merge("RGBA", (r_im,g_im,b_im,alp_im)) # menggabungkan kembali keempat gambar dari keempat array sebelumnya dengan mode RGBA
+    save_png.save("savepng.png")
+
+#img_comp_png("src/test_png.png",80)
 # img_comp("tes.jpg", 20)
 
 # from datetime import datetime
